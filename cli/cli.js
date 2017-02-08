056cf7c94ae56c7d41e4fb5f9ab24f1759153954
Kim mintae <kim.mintae@woowahan.com>
2017-02-08 15:54:29 +0900

    Adding a license file and modifying and adding the package.json entry

--- cli/cli.js
+++ cli/cli.js
@@ -2,30 +2,33 @@
 
 const ui = require('../helpers/ui');
 
-class CLI {
-	constructor(options) {
-		this.name = options.name;
-		this.root = options.root;
-		this.currentPath = options.currentPath;
-		this.inputStream = options.inputStream;
-    this.outputStream = options.outputStream;
-    this.errorStream = options.errorStream;
+module.exports = CLI;
+
+function CLI(options) {
+	this.name = options.name;
+	this.root = options.root;
+	this.currentPath = options.currentPath;
+	this.inputStream = options.inputStream;
+  this.outputStream = options.outputStream;
+  this.errorStream = options.errorStream;
+}
+
+function run(info) {
+	let args = info.args;
+
+	if(args.length === 0) {
+		args.push('help');
 	}
-	run(info) {
-		let command;
-		let args = info.args;
-		if(args.length === 0) {
-			args.push('help');
-		}
-		command = args[0];
-		this.command = command;
-		args.shift();
-		if(info.commands[command]) {
-			return info.commands[command].call(this, args);
-		} else {
-			return ui.console(ui.red('not exist command'));
-		}
+
+	this.command = args[0];
+
+	args.shift();
+
+	if(info.commands[this.command]) {
+		return info.commands[this.command].call(this, args);
+	} else {
+		return ui.console(ui.red('not exist command'));
 	}
 }
 
-module.exports = CLI;
+CLI.prototype.run = run;
