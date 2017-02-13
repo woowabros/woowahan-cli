7322331e918f342676f39416d961b707fd2de865
Kim mintae <kim.mintae@woowahan.com>
2017-02-13 18:33:14 +0900

    코드 리펙토링 1차

--- bin/woowahan-cli-new.js
+++ bin/woowahan-cli-new.js
@@ -1,24 +1,18 @@
 #!/usr/bin/env node
 'use strict';
 
+var path = require('path');
+var fs = require('fs');
 var program = require('commander');
+var chalk = require('chalk');
+var manifestManager = require('../lib/manifest-manager');
+var CLI = require('../lib/cli-task');
+
+const PROJECT_NAME = 0;
 
 program
 	.usage('[project-name] {option : git template name}')
 	.parse(process.argv);
-
-var path = require('path');
-var exists = require('fs').existsSync;
-var fs = require('fs-extra');
-var chalk = require('chalk');
-var projectName = program.args[0];
-var to = path.resolve('.');
-var projectPath = path.resolve(to, projectName);
-var cliPath = path.resolve(__dirname, '..');
-var packageJson = require('../lib/packageJson');
-var ora = require('ora');
-var exec = require('exec');
-var recursiveReadDirectory = require('recursive-readdir');
 
 program.on('--help' , function() {
 	console.log();
@@ -26,80 +20,26 @@ program.on('--help' , function() {
 	console.log();
 });
 
-if(exists(to)) {
-	var templatePath = path.resolve(cliPath, 'templates/app');
-	
-	packageJson(projectName)
-		.then(function(result) {
-			var customPackageObj = result;
-			var targetPackage  = path.resolve(projectPath , 'package.json');
-			var spinner = ora({
-				text: '',
-				spinner: {
-					interval: 100,
-					frames: [
-						'⠄ NPM Installing .',
-						'⠆ NPM Installing ..',
-						'⠇ NPM Installing ...',
-						'⠋ NPM Installing ....',
-						'⠙ NPM Installing .....',
-						'⠸ NPM Installing ......',
-						'⠰ NPM Installing .......',
-						'⠠ NPM Installing ......',
-						'⠰ NPM Installing .....',
-						'⠸ NPM Installing ....',
-						'⠙ NPM Installing ...',
-						'⠋ NPM Installing ..',
-						'⠇ NPM Installing .',
-						'⠆ NPM Installing '
-					]
-				}
-			});
+var CLIConfig = {};
 
-			if(fs.existsSync(projectPath)){
-				
-				console.log();
-				console.log(chalk.red(projectName + ' is already existed.'));
-				console.log();
+CLIConfig.projectName = program.args[PROJECT_NAME];
+CLIConfig.projectPath = path.resolve(__dirname, CLIConfig.projectName);
 
-			}else {
-				console.log();
-				console.log(chalk.cyan('initializing Woowahan App'));
+if (fs.existsSync(path.resolve(__dirname, CLIConfig.projectName))) {
+  console.log();
+  console.log(chalk.red(CLIConfig.projectName + ' is already existed.'));
+  console.log();
 
-				try{
-					
-					recursiveReadDirectory(templatePath, function (err, files) {
-						var filesLength = files.length;
-
-						for(var cnt =0; cnt < filesLength; cnt++) {
-							var file = files[cnt].replace(templatePath+'/' , '');
-
-							console.log(chalk.green(' create ') + file);
-						}
-						console.log();
-
-						fs.copySync(templatePath, projectPath);
-
-						fs.writeJsonSync(targetPackage, customPackageObj);
-
-						spinner.start();
-						process.chdir(projectPath);
-
-						exec('npm install', function(err, stdout, stderr) {
-							if(err !== null) {
-								spinner.stop();
-								console.log(chalk.yellow(err));
-							}
-							spinner.stop();
-							console.log(chalk.cyan('Successfully Installed!'));
-							console.log();
-						});
-
-					});
-				} catch(err) {
-					console.log(chalk.red('ERROR ' + err));
-				}
-
-			}	
-		});
+  process.exit();
 }
+
+Promise.resolve(CLIConfig)
+  .then(manifestManager)
+  .then(CLI.prepareWork)
+  .then(CLI.readTemplateFiles)
+  .then(CLI.writeTemplateFiles)
+  .then(CLI.npmInstall)
+  .then(CLI.finishWork)
+  .catch(function(err) {
+    console.error(chalk.yellow(err));
+  });
